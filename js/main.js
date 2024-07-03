120df150fc9dad6da74edf06897bc653ac14631e
ChikaDibia <ogbuchika71@gmail.com>
2024-07-03 01:19:51 +0100

    second commit

--- js/main.js
+++ js/main.js
@@ -13,12 +13,30 @@ window.addEventListener("DOMContentLoaded", () => {
   let day = days[date.getDay()];
   document.getElementById("span-day").innerHTML = day;
 
+  // Calls upDateTime function once when the page is fully loaded and then every second thereafter.
   function setAndUpDateTime() {
     const time = new Date();
     const currentTime = time.toLocaleTimeString();
     document.getElementById("span-time").innerHTML = currentTime;
   }
   setInterval(setAndUpDateTime, 1000); // Calls upDateTime every second
-  
 
-})
+  // Toggles the side menu on click of the open button
+  function openSideMenu() {
+    document.getElementById("sideMenu-container").style.width = `250px`;
+  }
+  document.getElementById("sideMenu-open-button").onclick = openSideMenu;
+
+  // Toggles the side menu off on click of the close button
+  function closeSideMenu() {
+    document.getElementById("sideMenu-container").style.width = `0`;
+  }
+  document.getElementById("sideMenu-close-button").onclick = closeSideMenu;
+
+  // Scrolls to the top of the page when the arrow is clicked in the footer
+  function scrollToTop() {
+    //  document.body.scrollTop = `1px`;
+    document.documentElement.scrollTop = `1px`;
+  }
+  document.getElementById("footer-arrow-up").onclick = scrollToTop;
+});
